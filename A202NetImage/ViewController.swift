127cf6d2d6189c7a472e3160f35d12ff77788d81
Danny Shen <shenfive@gmail.com>
2022-02-20 15:22:54 +0800

    init

--- A202NetImage/ViewController.swift
+++ A202NetImage/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var theImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let path = "https://attach.setn.com/newsimages/2019/12/28/2328596-PH.jpg"
+        let url = URL(string: path)!
+        
+        do {
+            let imageData = try Data(contentsOf: url)
+            self.theImageView.image = UIImage(data: imageData)
+        }catch{
+            print(error.localizedDescription)
+        }
+        
+        
     }
 
 
