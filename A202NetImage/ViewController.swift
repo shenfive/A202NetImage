f19b318963aca676594b081e4a3aa39db977da17
Danny Shen <shenfive@gmail.com>
2022-02-20 16:18:16 +0800

    Set View

--- A202NetImage/ViewController.swift
+++ A202NetImage/ViewController.swift
@@ -10,8 +10,26 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var theImageView: UIImageView!
     
+    @IBOutlet weak var content: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        theImageView.layer.borderColor = UIColor.red.cgColor
+        theImageView.layer.borderWidth = 3
+        theImageView.layer.cornerRadius = theImageView.bounds.width / 2
+        theImageView.clipsToBounds = true
+        
+        content.clipsToBounds = false
+        content.layer.cornerRadius = content.bounds.width / 2
+        content.layer.shadowColor = UIColor.gray.cgColor
+        content.layer.shadowRadius = 20         //陰影
+        content.layer.shadowOpacity = 0.6;
+        content.layer.shadowColor = UIColor.gray.cgColor
+        content.layer.shadowOffset = CGSize(width: 10, height: 10)
+
+        
+        
         
         let path = "https://attach.setn.com/newsimages/2019/12/28/2328596-PH.jpg"
         let url = URL(string: path)!
